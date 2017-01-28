6ec342676d654adc8aadad4eaffe820fa20de433
saferthanhouses <joey_oliver@hotmail.com>
2017-01-28 16:15:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sth/funfacts/FactBook.java
@@ -0,0 +1,30 @@
+package com.example.sth.funfacts;
+import java.util.Random;
+
+/**
+ * Created by sth on 27/01/17.
+ */
+
+public class FactBook {
+    // Fields (Member Variables) - object properties
+    private final String[] mFacts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built." };
+
+    // Methods - Actions the object can take
+    public String getFact(){
+        String fact = "";
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(mFacts.length);
+        fact = mFacts[randomNumber];
+        return fact;
+    };
+}
---
+++ app/src/main/java/com/example/sth/funfacts/FunFactsActivity.java
@@ -0,0 +1,48 @@
+package com.example.sth.funfacts;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+    // declare our View variables
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // assign the Views from the layout file to the right variables
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                int color = mColorWheel.getColor();
+                mRelativeLayout.setBackgroundColor(color);
+                mFactTextView.setText(fact);
+                mShowFactButton.setTextColor(color);
+            }
+        };
+
+        mShowFactButton.setOnClickListener(listener);
+//        Toast.makeText(FunFactsActivity.this, "Yay! Our activity was created", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We're logging from the onCreate method");
+    }
+}
